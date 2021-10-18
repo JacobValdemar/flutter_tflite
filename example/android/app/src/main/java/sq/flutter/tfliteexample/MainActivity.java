97cf6d216b5a4b80cb0cf7d358f234e25c2b2a94
JacobValdemar <Jacobvaldemarand@gmail.com>
2021-10-18 09:44:32 +0200

    mainactivity

--- example/android/app/src/main/java/sq/flutter/tfliteexample/MainActivity.java
+++ example/android/app/src/main/java/sq/flutter/tfliteexample/MainActivity.java
@@ -1,8 +1,9 @@
 package sq.flutter.tfliteexample;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 
 public class MainActivity extends FlutterActivity {
   @Override
